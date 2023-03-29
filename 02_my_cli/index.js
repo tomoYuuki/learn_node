5d856f063e6a1c41d127850af508c242bde99b0a
tomoYuuki <446679223@qq.com>
2023-03-29 16:20:24 +0800

    03-npm工具 添加命令 设置选项 配置参数 获取参数

--- 02_my_cli/index.js
+++ 02_my_cli/index.js
@@ -7,5 +7,23 @@ const program = new Command();
 // 设置命令的 version
 program.version(require("./package.json").version);
 
+// 设置选项
+// program.option("-w --why", "give a reason");
+program.option(
+  "-d --dest <dest>",
+  "a destination directory. egg: -d /src/component"
+);
+
+program.option("-f --framework <framework>", "switch framework .egg: -f react");
+
+// 监听命令
+program.on("--help", function (val) {
+  console.log("欢迎使用yuuki前端开发小工具");
+});
+
 // 使传来的参数让program可获取到
 program.parse(process.argv);
+
+// 获取自定义指令的参数
+const options = program.opts();
+console.log(options.dest);
