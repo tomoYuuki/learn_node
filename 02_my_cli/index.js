e75c0e3a252ae33646ad225043e9585d173f83f0
tomoYuuki <446679223@qq.com>
2023-03-29 16:29:47 +0800

    04-npm-抽离help指令

--- 02_my_cli/index.js
+++ 02_my_cli/index.js
@@ -1,25 +1,13 @@
 #!/usr/bin/env node
-
-const {Command} = require("commander");
-const program = new Command();
+const {program} = require("commander");
+const {helpOptions} = require("./lib/core/help");
 // 1.控制台下输入npm link 使 bin下的  命令与入口文件链接
 
 // 设置命令的 version
 program.version(require("./package.json").version);
 
-// 设置选项
-// program.option("-w --why", "give a reason");
-program.option(
-  "-d --dest <dest>",
-  "a destination directory. egg: -d /src/component"
-);
-
-program.option("-f --framework <framework>", "switch framework .egg: -f react");
-
-// 监听命令
-program.on("--help", function (val) {
-  console.log("欢迎使用yuuki前端开发小工具");
-});
+// 抽离help相关配置
+helpOptions();
 
 // 使传来的参数让program可获取到
 program.parse(process.argv);
---
+++ 02_my_cli/lib/core/help.js
@@ -0,0 +1,23 @@
+const {program} = require("commander");
+const helpOptions = () => {
+  // 设置选项
+  // program.option("-w --why", "give a reason");
+  program.option(
+    "-d --dest <dest>",
+    "a destination directory. egg: -d /src/component"
+  );
+
+  program.option(
+    "-f --framework <framework>",
+    "switch framework .egg: -f react"
+  );
+
+  // 监听命令
+  program.on("--help", function (val) {
+    console.log("欢迎使用yuuki前端开发小工具");
+  });
+};
+
+module.exports = {
+  helpOptions,
+};
