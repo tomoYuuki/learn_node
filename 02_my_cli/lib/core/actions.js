559b2b30f72dae69e9c86eeb1d3e727a0d4c78ac
tomoYuuki <446679223@qq.com>
2023-03-30 12:29:33 +0800

    增加提示语句

--- 02_my_cli/lib/core/actions.js
+++ 02_my_cli/lib/core/actions.js
@@ -4,6 +4,7 @@ const download = promisify(require("download-git-repo"));
 const {vueRepo} = require("../config/repo-config.js");
 // const {commandSpawn} = require("../utils/terminal");
 const createProjectAction = async (project, others) => {
+  console.log("yuuki help you build your project");
   // clone项目 至当前文件夹的${project}下
   await download(vueRepo, project, {clone: true});
 
